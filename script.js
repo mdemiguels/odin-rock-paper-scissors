6779569fab2bba08f3f0d1b3062ac3d0b7122933
mdemiguels <miguelschz1997@gmail.com>
2024-09-16 21:10:43 +0200

    add computer random choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+
+function getComputerChoice() {
+
+    const randomChoice = parseInt(Math.random() * 3);
+
+    if(randomChoice === 0) return 'rock';
+    if(randomChoice === 1) return 'paper';
+    if(randomChoice === 2) return 'scissors';
+
+}
+
+console.log(getComputerChoice())
+
