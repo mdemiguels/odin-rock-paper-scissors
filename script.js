027ee330daaa442f9ab77b30b6a5ed9fc89cbdf1
mdemiguels <miguelschz1997@gmail.com>
2024-09-23 13:58:59 +0200

    refactor result representation on html with js

--- script.js
+++ script.js
@@ -1,78 +1,92 @@
 let computerScore = 0;
 let humanScore = 0;
+let humanSelection = "";
 
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
 
-const resultContainer = document.querySelector('.result-container');
+const resultContainer = document.querySelector(".global-result-container");
+const roundContainer = document.querySelector(".round-result-container");
 
 document.addEventListener("DOMContentLoaded", (evt) => {
-        printResult();
+    printGlobalResult();
 });
-  
+
 rockBtn.addEventListener("click", (evt) => {
-    console.log("you clicked rock!");
-    playGame("rock");
+    // console.log("you clicked rock!");
+    humanSelection = "rock";
+    playGame();
 });
 paperBtn.addEventListener("click", (evt) => {
-    console.log("you clicked paper!");
-    playGame("paper");
+    // console.log("you clicked paper!");
+    humanSelection = "paper";
+    playGame();
 });
 scissorsBtn.addEventListener("click", (evt) => {
-    console.log("you clicked scissors!");
-    playGame("scissors");
+    // console.log("you clicked scissors!");
+    humanSelection = "scissors";
+    playGame();
 });
 
-function playGame(humanSelection) {
-
-    let computerSelection =  getComputerSelection();
-
-    playRound(humanSelection, computerSelection);
-    printResult();
+function playGame() {
+    playRound();
 }
 
-function printResult() {
-
+function printGlobalResult() {
     if (resultContainer.lastChild) {
         resultContainer.removeChild(resultContainer.lastChild);
     }
 
-    const scoreParagraph = document.createElement('p');
+    const scoreParagraph = document.createElement("p");
     scoreParagraph.textContent = `human: ${humanScore} - computer: ${computerScore}`;
 
     resultContainer.appendChild(scoreParagraph);
-
-    if (humanScore === 5 || computerScore === 5) {
-
-        if (humanScore > computerScore)
-            console.log("You are the winner!! Congrats!! 🎊");
-        else if (humanScore < computerScore)
-            console.log("You have lost VS a computer... Shame on you 🤖");
-        else console.log("It's Tie! Could be worse... 😜");
-
-    }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`It\'s tie! both chose ${humanChoice}`);
+function printRoundResult(msg) {
+
+    if (roundContainer.lastChild) {
+        roundContainer.removeChild(roundContainer.lastChild);
+    }
+
+    const roundParagraph = document.createElement("p");
+    roundParagraph.textContent = msg;
+
+    roundContainer.appendChild(roundParagraph);
+
+}
+
+function playRound() {
+    let computerSelection = getComputerSelection();
+
+    if (humanSelection === computerSelection) {
+        // console.log(`It\'s tie! both chose ${humanSelection}`);
+        printRoundResult(`It\'s tie! both chose ${humanSelection}`);
         return;
     }
 
-    if (humanChoice === "rock" && computerChoice === "scissors") {
+    if (humanSelection === "rock" && computerSelection === "scissors") {
         humanScore++;
-        console.log("You win! rock beats scissors");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        // console.log("You win! rock beats scissors");
+        printRoundResult("You win! rock beats scissors");
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
         humanScore++;
-        console.log("You win! paper beats rock");
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        // console.log("You win! paper beats rock");
+        printRoundResult("You win! paper beats rock");
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
         humanScore++;
-        console.log("You win! scissors beats paper");
+        // console.log("You win! scissors beats paper");
+        printRoundResult("You win! scissors beats paper");
     } else {
         computerScore++;
-        console.log(`You loose! ${computerChoice} beats ${humanChoice}`);
+        // console.log(`You loose! ${computerSelection} beats ${humanSelection}`);
+        printRoundResult(
+            `You loose! ${computerSelection} beats ${humanSelection}`
+        );
     }
+
+    printGlobalResult();
 }
 
 function getComputerSelection() {
